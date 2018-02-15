92eb1d58bc9e6c14b5223384ea4919c73efb73a4
Jess <jsscthomp@gmail.com>
2018-02-15 16:42:44 -0500

    added routing to search box

--- glogetter/src/components/SearchBox.js
+++ glogetter/src/components/SearchBox.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBox extends React.Component {
+    constructor() {
+        super();
+        this.goToProduct = this.goToProduct.bind(this);
+    }
+
+    goToProduct(event) {
+        event.preventDefault();
+        console.log("you've searched for a product");
+        //grab text from search box
+        const productId = this.productInput.value;
+        console.log(`Going to ${productId} page`);
+        //transition from/to /product/:productId
+        this.context.router.transitionTo(`/product/${productId}`);
+    }
+
     render() {
         return (
-            <form className='term-search' onSubmit={this.goToTerm}>
-                <h2>Glo Get It</h2>
+            <form className='product-search' onSubmit={this.goToProduct}>
+                <h2>Glo Get It...</h2>
                 <input type='text' required placeholder='Search' defaultValue='' ref={(input) => { this.productInput = input }} />
                 <button type='submit'>Go!</button>
             </form>
         )
     }
+}
+
+SearchBox.contextTypes = {
+    router: PropTypes.object
 }
 
 export default SearchBox;
