6cb21b464b06f8767f57bbd5d2f8ecf8aead75e7
Jess <jsscthomp@gmail.com>
2018-02-16 18:39:53 -0500

    added ProductBox component

---
+++ glogetter/src/components/ProductBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ProductBox extends React.Component {
+    render() {
+        return {
+            console.log('this is a box with a product in it');
+        }
+    }
+}
+
+export default ProductBox;
